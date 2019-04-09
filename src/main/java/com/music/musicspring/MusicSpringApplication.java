2fa6a4ca17eb5387042937d3097f4567a52de8e7
Nekokir <2195751533@qq.com>
2019-04-09 13:29:12 +0800

    idea

---
+++ src/main/java/com/music/musicspring/MusicSpringApplication.java
@@ -0,0 +1,13 @@
+package com.music.musicspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicSpringApplication.class, args);
+    }
+
+}
